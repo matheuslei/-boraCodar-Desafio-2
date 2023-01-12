1f9b58b09e9af1b1f87789b20b6ade72b1ce714e
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2023-01-12 12:08:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function trocaImg(imagem) {
   let id = document.getElementById("imagem");
   if ((imagem = id)) {
-    document.getElementById("imagem").src = "./img/sofa2.gif";
+    document.getElementById("imagem").src = "https://media3.giphy.com/media/7slrmFPHImqFqimyX3/giphy.gif?cid=790b76119c735ab92d408f7fa7ea640f20607f5121bf221c&rid=giphy.gif&ct=g";
     document.getElementById("imagem").id = "img1";
     document.querySelector("button").innerHTML = "ADICIONADO";
     document.querySelector("button").style.background = "#9085ab";
